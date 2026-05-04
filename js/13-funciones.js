c517e800527dba1d3db4795f7370770bd97124d2
coluladianaeva-cell <coluladianaeva@gmail.com>
2026-05-04 10:57:05 -0600

    aprendimos los tres tipos de funciones y sus parametros y/o argumentos

---
+++ js/13-funciones.js
@@ -0,0 +1,109 @@
+/* 
+
+Funciones.
+
+Las funciones de codigo reutilizables que realizan la tarea especifica. 
+
+1. Funcion Declarada 
+
+Es la forma mas comun de definir funciones. Se puede llamra a la funcion antes de su definicion debido al hoisting, es decir se "eleva" en el contexto de ejecucion. 
+
+Sintaxis:
+
+function nombreFuncion() {
+// Cuerpa de la funcion
+...
+}
+
+llamada o invocacion de la funcion:
+
+nombreFuncion();
+
+*/
+
+dianaSaluda()
+
+function dianaSaluda() {
+  console.log("Hola soy diana y tengo siempre sueño");
+}
+
+/* 
+
+2. Funcion expresada (anonimas)
+
+Este tipo de funcion se asignan a una variable. No puede ser llamada antes de su definicion.
+
+Sintaxis:
+
+const nombreConstante = function() {
+//Cuerpa de la funcion 
+...
+}
+
+Llamada de la funcion:
+
+nombreConstante();
+
+*/
+
+const joshuaDespedida = function () {
+  console.log("Adios vaquero🤠");
+}
+
+joshuaDespedida();
+
+/* 
+
+3. Funcion flecha = Arrow Function 
+
+Tienen una sitaxis mas consisa, son utiles para ejecucion cortas. Generalmente se asignan a variables.
+
+Sintaxis: 
+
+const nombreConstante = () => {
+  //Cuerpa de la funcion
+  ...
+}
+
+Llamada
+
+nombreConstante();
+
+*/
+
+const astridSos = () => { console.log("Auxilio muero de hambre 🤤") };
+
+astridSos();
+
+/* 
+
+Parametros y argumentos
+
+Las funciones pueden aceptar parametros para recibir datos cuando son invocados.
+
+Sintaxis:
+
+function nombreFuncion( parametro1 , parametro2 , ...) {
+//Cuerpa de la funcion
+// Los parametros los usamos dentro del cuerpo de la funcion
+...
+}
+
+Llamadas:
+
+nombreFuncion( argumento1, argumento2, ...);
+
+*/
+
+function saludar(nombre) {
+  console.log("Hola" + nombre);
+}
+
+saludar("Dianita");
+
+const sumar = function (num1, num2) {
+  let suma = num1 + num2;
+  console.log("El resultado es" + suma);
+}
+
+sumar(5, 10);
